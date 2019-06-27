dbb499d5af1e8e5d0a997982275235f628bb79d9
Martin Štěpánek <mstepanek015@gmail.com>
2019-06-27 14:11:49 +0200

    Fix ci.php

--- ci.php
+++ ci.php
@@ -1,18 +1,19 @@
 <?php
-// run phpmetrics to generate report
-exec('"./vendor/bin/phpmetrics" . --report-html="./web/report" --exclude="vendor","web","runtime","migrations"');
 
-function removeDirectory($path) {
+function removeDirectoryContent($path) {
     $files = glob($path . '/*');
     foreach ($files as $file) {
-        is_dir($file) ? removeDirectory($file) : unlink($file);
+        is_dir($file) ? removeDirectoryContent($file) : unlink($file);
     }
-    rmdir($path);
-    return;
 }
 
 // remove cache directory (because of documentation)
-removeDirectory('runtime/cache');
+if(file_exists('runtime/cache')) {
+    removeDirectoryContent('runtime/cache');
+}
 
-exec('composer install --no-interaction');
+exec('php /usr/local/bin/composer install --verbose --no-interaction');
 exec('php yii migrate --interactive=0');
+
+// run phpmetrics to generate report
+exec('"./vendor/bin/phpmetrics" . --report-html="./web/report" --exclude="vendor","web","runtime","migrations"');
